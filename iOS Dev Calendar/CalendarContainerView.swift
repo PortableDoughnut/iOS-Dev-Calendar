26a09481dca167e7f889713f9d8f7992d72a9703
Wesley Keetch <keetchcode@gmail.com>
2025-04-17 09:11:38 -0600

    Still working on CalendarView, will refactor to include taskView inisde of the calendarView so we only have 3 tabs instead of 4. Will import boilerplate demo code from the mijick calendar package.

--- iOS Dev Calendar/CalendarContainerView.swift
+++ iOS Dev Calendar/CalendarContainerView.swift
@@ -11,23 +11,44 @@ import MijickCalendarView
 
 struct CalendarContainerView: View {
   @Binding var selectedDate: Date?
-  @Binding var selectedRange: MDateRange?
-  
+  let availableDates: [CalendarDate]
+  @Binding var showAllDates: Bool
+
   var body: some View {
-    MCalendarView(selectedDate: $selectedDate, selectedRange: $selectedRange)
+    MCalendarView(
+      selectedDate: $selectedDate,
+      selectedRange: .constant(nil)
+    ) { config in
+      config
+        .startMonth(Calendar.current.date(from: DateComponents(year: 2025, month: 1, day: 1)) ?? Date())
+        .endMonth(Calendar.current.date(from: DateComponents(year: 2025, month: 12, day: 31)) ?? Date())
+        .firstWeekday(.sunday)
+        .monthsSpacing(24)
+        .daysVerticalSpacing(8)
+        .daysHorizontalSpacing(4)
+        .monthsViewBackground(Color(.systemBackground))
+        .dayView { date, isCurrentMonth, selectedDateBinding, selectedRangeBinding in
+          CustomDayView(
+            date: date,
+            isCurrentMonth: isCurrentMonth,
+            selectedDate: selectedDateBinding,
+            selectedRange: selectedRangeBinding,
+            availableDates: availableDates
+          )
+        }
+    }
   }
 }
 
 #if DEBUG
-import SwiftUI
-
 struct CalendarContainerView_Previews: PreviewProvider {
   static var previews: some View {
-    CalendarContainerView(
+    let previewDates = CSVDateLoader.loadDatesFromCSV()
+    return CalendarContainerView(
       selectedDate: .constant(nil),
-      selectedRange: .constant(MDateRange())
+      availableDates: previewDates,
+      showAllDates: .constant(true)
     )
-    .previewLayout(.sizeThatFits)
   }
 }
 #endif
--- iOS Dev Calendar/Controller/CalendarViewController.swift
+++ iOS Dev Calendar/Controller/CalendarViewController.swift
@@ -10,36 +10,128 @@ import SwiftUI
 import MijickCalendarView
 
 class CalendarViewController: UIViewController {
-  private var selectedDate: Date? = nil
-  private var selectedRange: MDateRange? = .init()
-
+  private var selectedDate: Date? = nil {
+    didSet {
+      if let date = selectedDate {
+        showDetailForDate(date)
+      }
+    }
+  }
+  
+  private var availableDates: [CalendarDate] = []
+  private var showAllDates: Bool = true
+  private var currentFilter: DayType?
+  private var hostingController: UIHostingController<CalendarContainerView>?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
+    // Configure navigation bar
+    navigationController?.navigationBar.prefersLargeTitles = false
+    title = "iOS Dev Calendar"
+    
+    // Load dates from CSV
+    availableDates = CSVDateLoader.loadDatesFromCSV()
+    
+    // Create filter menu
+    setupFilterMenu()
+    
+    setupCalendarView()
+  }
+  
+  private func setupFilterMenu() {
+    let filterImage = UIImage(systemName: "line.3.horizontal.decrease.circle")
+    
+    // Create menu items for each unit type
+    var menuItems: [UIMenuElement] = []
+    
+    // Add "Show All" option
+    menuItems.append(UIAction(title: "Show All", image: UIImage(systemName: "calendar"), handler: { [weak self] _ in
+      self?.showAllDates = true
+      self?.currentFilter = nil
+      self?.setupCalendarView()
+    }))
+    
+    // Add separator
+    let unitActions = [
+      (title: "Swift Fundamentals (SF)", type: DayType.sf, image: "swift"),
+      (title: "Technical Projects (TP)", type: DayType.tp, image: "hammer"),
+      (title: "Network & Data (ND)", type: DayType.nd, image: "network"),
+      (title: "SwiftUI & Tools (ST)", type: DayType.st, image: "macwindow"),
+      (title: "Testing & Tools (TT)", type: DayType.tt, image: "checklist"),
+      (title: "Final Apps (FA)", type: DayType.fa, image: "app"),
+      (title: "Programming Concepts (PC)", type: DayType.pc, image: "book"),
+      (title: "General Concepts (GC)", type: DayType.gc, image: "gear")
+    ]
+    
+    let unitMenuItems = unitActions.map { action in
+      UIAction(
+        title: action.title,
+        image: UIImage(systemName: action.image),
+        handler: { [weak self] _ in
+          self?.filterByType(action.type)
+        }
+      )
+    }
+    
+    menuItems.append(UIMenu(title: "Units", options: .displayInline, children: unitMenuItems))
+    
+    let menu = UIMenu(title: "Filter", image: filterImage, children: menuItems)
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, image: filterImage, primaryAction: nil, menu: menu)
+  }
+  
+  private func filterByType(_ type: DayType) {
+    showAllDates = false
+    currentFilter = type
+    setupCalendarView()
+  }
+  
+  private func setupCalendarView() {
+    // Remove existing hosting controller if any
+    hostingController?.removeFromParent()
+    hostingController?.view.removeFromSuperview()
+    
+    let filteredDates = showAllDates ? availableDates : availableDates.filter {
+      // Don't filter out holidays when showing specific unit
+      DayType.from($0.label) == currentFilter || DayType.from($0.label) == .holiday
+    }
+    
     let calendarView = CalendarContainerView(
       selectedDate: Binding(
         get: { self.selectedDate },
         set: { self.selectedDate = $0 }
       ),
-      selectedRange: Binding(
-        get: { self.selectedRange },
-        set: { self.selectedRange = $0 }
+      availableDates: filteredDates,
+      showAllDates: Binding(
+        get: { self.showAllDates },
+        set: { self.showAllDates = $0 }
       )
     )
-
-    let hostingController = UIHostingController(rootView: calendarView)
-    addChild(hostingController)
-    hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-    view.addSubview(hostingController.view)
-
+    
+    let newHostingController = UIHostingController(rootView: calendarView)
+    hostingController = newHostingController
+    
+    addChild(newHostingController)
+    newHostingController.view.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(newHostingController.view)
+    
     NSLayoutConstraint.activate([
-      hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-      hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-      hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+      newHostingController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      newHostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+      newHostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      newHostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
     ])
-
-    hostingController.didMove(toParent: self)
+    
+    newHostingController.didMove(toParent: self)
+  }
+  
+  private func showDetailForDate(_ date: Date) {
+    guard let calendarDate = availableDates.first(where: { Calendar.current.isDate($0.date, inSameDayAs: date) }) else {
+      return
+    }
+    
+    let detailViewController = CalendarDetailViewController(calendarDate: calendarDate)
+    navigationController?.pushViewController(detailViewController, animated: true)
   }
 }
 
@@ -57,7 +149,7 @@ struct CalendarViewControllerRepresentable: UIViewControllerRepresentable {
   func makeUIViewController(context: Context) -> CalendarViewController {
     return CalendarViewController()
   }
-
+  
   func updateUIViewController(_ uiViewController: CalendarViewController, context: Context) {
   }
 }
