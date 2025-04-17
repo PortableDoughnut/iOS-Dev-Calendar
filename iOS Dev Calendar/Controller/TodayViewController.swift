17bf740f5c2716fcd5b227395d9dde173c81202d
PortableDoughnut <gwen.thelin@gmail.com>
2025-04-17 13:19:48 -0600

    Set up firrst design for Today View: * Put TodayView in Navigatoin Stack * Created CardView and CardModel for displaying information * Created Template Cards for the TodayView

--- iOS Dev Calendar/Controller/TodayViewController.swift
+++ iOS Dev Calendar/Controller/TodayViewController.swift
@@ -8,22 +8,95 @@
 import UIKit
 
 class TodayViewController: UIViewController {
-
-    override func viewDidLoad() {
+	@IBOutlet weak var CodeChallengeCard: CardView!
+	@IBOutlet weak var TonightsHomeworkView: CardView!
+	@IBOutlet weak var DueHomeworkCardView: CardView!
+	@IBOutlet weak var ReviewCardView: CardView!
+	@IBOutlet weak var GoalCardView: CardView!
+	@IBOutlet weak var TeacherCardView: CardView!
+	@IBOutlet weak var LessonCardView: CardView!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+	
+		setupTitles()
+		setupCards()
+	}
+	
+		/// Sets up the navigaion titles in a seprate funciton to keep things clean
+	func setupTitles() {
+		navigationController?.navigationBar.prefersLargeTitles = true
+		navigationItem.largeTitleDisplayMode = .always
+		
+		let formatter = DateFormatter()
+		formatter.dateStyle = .long
+		formatter.timeStyle = .none
+		let dateString = formatter.string(from: Date())
+		
+		navigationItem.title = dateString
+	}
+	
+		/// Fills the card outlets with data
+	func setupCards() {
+		for (card, model) in [
+			(
+				LessonCardView,
+				CardModel(
+					title: "Lesson",
+					subtitle: "SOLID",
+					backgroundColor: UIColor(red: 0.43, green: 0.11, blue: 0.14, alpha: 1.0)
+				)
+			),
+			(
+			TeacherCardView,
+			CardModel(
+				title: "Teacher",
+				subtitle: "Gwen Thelin",
+				backgroundColor: UIColor(red: 0.95, green: 0.79, blue: 0.41, alpha: 1.0)
+			)
+			),
+			(
+			GoalCardView,
+			CardModel(
+				title: "Goal",
+				subtitle: "Learn SOLID principles",
+				backgroundColor: UIColor(red: 0.17, green: 0.18, blue: 0.26, alpha: 1.0)
+			)
+		),
+			(
+			ReviewCardView,
+			CardModel(
+				title: "Review",
+				subtitle: "Review notes from yesterday",
+				backgroundColor: UIColor(red: 0.55, green: 0.55, blue: 0.54, alpha: 1.0)
+			)
+			),
+			(
+				DueHomeworkCardView,
+				CardModel(
+					title: "Homework Due",
+					subtitle: "Emoji Dictionary",
+					backgroundColor: UIColor(red: 0.84, green: 0.62, blue: 0.60, alpha: 1.0)
+				)
+			),
+			(
+				TonightsHomeworkView,
+				CardModel(
+					title: "Tonight's Homework",
+					subtitle: "Social Media App",
+					backgroundColor: UIColor(red: 0.95, green: 0.84, blue: 0.84, alpha: 1.0)
+				)
+			),
+			(
+				CodeChallengeCard,
+				CardModel(
+					title: "Code Challenge",
+					subtitle: "Build a Calculator",
+					backgroundColor: UIColor(red: 0.30, green: 0.42, blue: 0.31, alpha: 1.0)
+				)
+			)
+		] {
+			card?.configureWithModel(model)
+		}
+	}
 }
